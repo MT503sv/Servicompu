c103b9850720d2b687c1c1e213ba5f9e0189c509
Mateo PC <mateoantoniomenjivarmelara@gmail.com>
2025-11-08 23:05:51 -0600

    chages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true, 
-  },
+  output: 'export', 
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
